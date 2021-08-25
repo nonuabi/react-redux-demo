cb4e8549b7bae0a6400419185a14812ed47c386d
nonuabi <nonuabi@gmail.com>
2021-08-25 19:12:14 +0530

    store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import CakeContainer from "./components/CakeContainer";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 const App = () => {
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
   );
 };
 
