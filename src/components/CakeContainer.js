ba93599589bcbb1b6d324781da77b17f3d08d082
nonuabi <nonuabi@gmail.com>
2021-08-25 19:02:36 +0530

    react app setup

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CakeContainer = () => {
+  return (
+    <div>
+      <h2>Number of Cakes</h2>
+      <button>Buy Cakes</button>
+    </div>
+  );
+};
+
+export default CakeContainer;
